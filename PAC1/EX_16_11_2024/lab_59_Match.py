07e8e2d06b515293ed4b5168dd0eb4bf5eb938a2
satya <murthy.kandavalli@gmail.com>
2024-11-19 07:07:14 +0530

    November 16th

--- PAC1/EX_16_11_2024/lab_59_Match.py
+++ PAC1/EX_16_11_2024/lab_59_Match.py
@@ -2,16 +2,17 @@
 # it is like switch in java
 # match the op and execute
 # python  > 3.10
+from unittest import case
 
 BROWSER_NAME = input(" ENTERBTHE BROWSER NAME:\n")
 match BROWSER_NAME:
     case "FIREFOX":
         print("Starting FIREFOX")
-        case "CHROME":
+    case "CHROME":
         print("Starting CHROME")
-        case "SAFARI":
+    case "SAFARI":
         print("Starting SAFARI")
-        case "EDGE":
+    case "EDGE":
         print("Starting EDGE")
-        case_:
+    case "_":
         print("Browser not found")
